e467c1cffd19f8fc369fe2f519a395355eb2f41d
Victor Duarte <victor.slduarte@gmail.com>
2018-11-06 15:28:02 -0200

    ao inves de fazer um controller pra user, usei o proprio controler da gem devise. Alem disso, abri as view de user atravez da gem e adicionei strong params p App controller para fazer rodar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:fisrt_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
