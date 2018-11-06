60ed8df9ff1dd523db9426f299561422163db988
Bruno Passos <bgpassos@gmail.com>
2018-11-06 14:44:32 -0200

    Defs index, show and create of Categories Controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,39 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category =Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
