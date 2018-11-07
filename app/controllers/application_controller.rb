75c5ff2fd1a8ca5b914d68e63a527b57e07084a0
Victor Duarte <victor.slduarte@gmail.com>
2018-11-07 13:03:17 -0200

    sign in, sing up, edit, tudo ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name, :adress, :phone])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :phone, :email, :username, :password, :password_confirmation, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name, :address, :phone])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_11_06_173820) do
-=======
-ActiveRecord::Schema.define(version: 2018_11_07_132235) do
->>>>>>> 6b4498842fb4dbf7688689225298e07683e02119
+ActiveRecord::Schema.define(version: 2018_11_07_140207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +64,7 @@ ActiveRecord::Schema.define(version: 2018_11_07_132235) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "products", "categories"
