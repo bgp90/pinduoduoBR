13d84a8b2bb186b83abe8d5d3326ac6d2145b1eb
diego de paula <diegodepaula@hotmail.com>
2018-11-07 15:53:42 -0200

    Create seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+puts "Create 10 categories..."
+10.times do
+  category = Category.create(
+    name: Faker::Cannabis.category
+  )
+end
+puts "Done creating categories"
+
+
+puts "Create 10 products..."
+10.times do
+  product = Product.create(
+    name: Faker::Cannabis.strain,
+    description: Faker::Cannabis.medical_use,
+    supplier: Faker::Cannabis.type,
+    current_price: rand(0..50),
+    avg_price: rand(0..50),
+    price_goal: rand(0..50),
+    sold_count: rand(0..50),
+    avaiable_items: rand(0..50),
+    category_id: rand(0..10),
+    initial_price: rand(0..50)
+  )
+end
+
+puts "Done creating products"
