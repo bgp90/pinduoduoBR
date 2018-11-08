53bd0d82495bc7bf61b14549b9c80dd13d3e7f4d
Victor Duarte <victor.slduarte@gmail.com>
2018-11-08 16:46:25 -0200

    image1/2 to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_211218) do
+ActiveRecord::Schema.define(version: 2018_11_08_183946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2018_11_07_211218) do
     t.datetime "updated_at", null: false
     t.bigint "category_id"
     t.decimal "initial_price"
+    t.string "image1"
+    t.string "image2"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
